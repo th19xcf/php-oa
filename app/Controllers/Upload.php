ae330d63bd7a9b9cb6fc67eda5a129af0a59b5ca
th19xcf <th19xcf@gmail.com>
2022-04-29 23:35:02 +0800

    2022-4-29

--- app/Controllers/Upload.php
+++ app/Controllers/Upload.php
@@ -1,5 +1,5 @@
 <?php
-/* v1.1.3.1.202204291510, from office */
+/* v1.1.4.1.202204292330, from home */
 
 namespace App\Controllers;
 use \CodeIgniter\Controller;
@@ -86,6 +86,8 @@ class Upload extends Controller
         $sheet_data = $sheet->toArray(true, true, true, true, true);
 
         //数据库操作
+        $tmp_table_name = sprintf('tmp_%s_%s_%s_%s', $menu_id, $menu_1, $menu_2, $user_workid);
+
         $sql = sprintf(
                 'select 列名,字段名,字段类型,字段长度,
                     赋值类型,对象,导入类型,系统变量
@@ -96,7 +98,6 @@ class Upload extends Controller
         $query = $model->select($sql);
         $results = $query->getResult();
 
-        $table_name = sprintf('tmp_%s_%s_%s_%s', $menu_id, $menu_1, $menu_2, $user_workid);
         $fld_arr = [];
         $fld_ceate_str = '';
 
@@ -108,14 +109,68 @@ class Upload extends Controller
             $fld_ceate_str = sprintf('%s %s varchar(%s) not null default ""', $fld_ceate_str, $row->字段名, $row->字段长度);
         }
 
-        $sql = sprintf('drop table if exists %s;', $table_name);
+        $sql = sprintf('drop table if exists %s;', $tmp_table_name);
         $rc = $model->exec($sql);
 
         $sql = sprintf('create table %s (%s);', 
-            $table_name, $fld_ceate_str);
+            $tmp_table_name, $fld_ceate_str);
         $rc = $model->exec($sql);
 
-        $model->add($table_name, $sheet_data, $fld_arr);
+        $model->add($tmp_table_name, $sheet_data, $fld_arr);
+
+        //数据校验
+
+        // 插入正式表
+        $sql = sprintf('select 表名 from def_import_config
+            where 导入模块="%s"', $import);
+
+        $query = $model->select($sql);
+        $results = $query->getResult();
+
+        $dest_table_name = '';
+        foreach ($results as $row)
+        {
+            $dest_table_name = $row->表名;
+            break;
+        }
+
+        $sql = sprintf(
+            'select 列名,字段名,字段类型,字段长度,
+                赋值类型,对象,导入类型,
+                replace(系统变量," ","") as 系统变量
+            from def_import_column
+            where 导入模块="%s"', $import);
+
+        $query = $model->select($sql);
+        $results = $query->getResult();
+
+        $tmp_fld_arr = [];
+        $dest_fld_arr = [];
+
+        foreach ($results as $row)
+        {
+            array_push($dest_fld_arr, $row->字段名);
+            switch ($row->系统变量)
+            {
+                case '':
+                    array_push($tmp_fld_arr, $row->字段名);
+                    break;
+                case '$工号':
+                    array_push($tmp_fld_arr, sprintf('"%s" as %s', $user_workid, $row->字段名));
+                    break;
+                case '$时间戳':
+                    array_push($tmp_fld_arr, sprintf('"" as %s', $row->字段名));
+                    break;
+                default:
+                    array_push($tmp_fld_arr, sprintf('"%s" as %s', $row->系统变量, $row->字段名));
+                    break;
+            }
+        }
+
+        $tmp_fld_str = implode(',', $tmp_fld_arr);
+        $dest_fld_str = implode(',', $dest_fld_arr);
+        $sql_insert = sprintf('insert into %s (%s) select %s from %s', $dest_table_name, $dest_fld_str, $tmp_fld_str, $tmp_table_name);
+        $rc = $model->exec($sql_insert);
 
         $this->json_data(200, '导入成功', 0);
     }
