b5fb9a849aa140183646c9b991e8711800027840
th19xcf <th19xcf@gmail.com>
2022-05-31 20:36:24 +0800

    2022-5-31

--- app/Controllers/Scheduling.php
+++ app/Controllers/Scheduling.php
@@ -1,5 +1,6 @@
 <?php
-/* v1.0.1.1.202205282355, from home */
+/* v1.1.1.1.202205311600, from office */
+
 namespace App\Controllers;
 use \CodeIgniter\Controller;
 use App\Models\Mcommon;
@@ -10,12 +11,16 @@ class Scheduling extends Controller
     {
         helper(['form', 'url']);
     }
-
-    public function init()
+ 
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    // 排班
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    public function init($menu_id='')
     {
+        $model = new Mcommon();
+
         $sql = sprintf('select 业务,班组,姓名,工号 from view_排班人员');
 
-        $model = new Mcommon();
         $query = $model->select($sql);
         $results = $query->getResult();
 
@@ -36,56 +41,80 @@ class Scheduling extends Controller
             if ($team_last == '')
             {
                 $team_last = $row->班组;
-                $team_arr['id'] = $row->班组;
+                $team_arr['id'] = '2级^' . $row->班组;
                 $team_arr['value'] = $row->班组;
                 $team_arr['items'] = [];
 
                 $biz_last = $row->业务;
-                $biz_arr['id'] = $row->业务;
+                $biz_arr['id'] = '1级^' . $row->业务;
                 $biz_arr['value'] = $row->业务;
                 $biz_arr['items'] = [];
             }
 
             $ee_arr = array();
-            $ee_arr['id'] = sprintf('%s_%s', $row->姓名, $row->工号);
+            $ee_arr['id'] = sprintf('3级^%s_%s', $row->姓名, $row->工号);
             $ee_arr['value'] = sprintf('%s (%s)', $row->姓名, $row->工号);
 
-            if ($team_last == $row->班组)
+            if ($team_last == $row->班组 && $ii < (count($results)-1))
             {
                 array_push($team_arr['items'], $ee_arr);
                 $team_ee_num ++;
+            }
+            if ($team_last != $row->班组 && $ii < (count($results)-1))
+            {
+                // 新班组,老班组信息保存
+                $team_arr['value'] = sprintf('%s  (%d人)', $team_arr['value'], $team_ee_num);
+                $biz_ee_num = $biz_ee_num + $team_ee_num;
+                $team_ee_num = 0;
 
-                // 处理最后一类
-                if ($ii < (count($results)-1)) continue;
+                if ($biz_last == $row->业务)
+                {
+                    array_push($biz_arr['items'], $team_arr);
+                }
+                else
+                {
+                    array_push($biz_arr['items'], $team_arr);
+                    $biz_arr['value'] = sprintf('%s  (%d人)', $biz_arr['value'], $biz_ee_num);
+                    $biz_ee_num = 0;
+    
+                    array_push($csr_arr, $biz_arr);
+    
+                    $biz_last = $row->业务;
+                    $biz_arr['id'] = '1级^' . $row->业务;
+                    $biz_arr['value'] = $row->业务;
+                    $biz_arr['items'] = [];    
+                }
+
+                // team信息初始化
+                $team_last = $row->班组;
+                $team_arr['id'] = '2级^' . $row->班组;
+                $team_arr['value'] = $row->班组;
+                $team_arr['items'] = [];
+
+                array_push($team_arr['items'], $ee_arr);
+                $team_ee_num ++;
             }
 
-            // 新班组,老班组信息保存
-            $team_arr['value'] = sprintf('%s  (%d人)', $team_arr['value'], $team_ee_num);
-            $biz_ee_num = $biz_ee_num + $team_ee_num;
-            $team_ee_num = 0;
-
-            if ($biz_last == $row->业务)
+            if ($team_last == $row->班组 && $ii == (count($results)-1))
             {
-                array_push($biz_arr['items'], $team_arr);
-            }
+                // 二级
+                array_push($team_arr['items'], $ee_arr);
+                $team_ee_num ++;
 
-            // team信息初始化
-            $team_last = $row->班组;
-            $team_arr['id'] = $row->班组;
-            $team_arr['value'] = $row->班组;
-            $team_arr['items'] = [];
+                $team_arr['value'] = sprintf('%s  (%d人)', $team_arr['value'], $team_ee_num);
+                $biz_ee_num = $biz_ee_num + $team_ee_num;
 
-            if ($biz_last != $row->业务 || $ii == (count($results)-1))
-            {
+                // 一级
                 $biz_arr['value'] = sprintf('%s  (%d人)', $biz_arr['value'], $biz_ee_num);
                 $biz_ee_num = 0;
+                array_push($biz_arr['items'], $team_arr);
 
+                // 最终
                 array_push($csr_arr, $biz_arr);
+            }
 
-                $biz_last = $row->业务;
-                $biz_arr['id'] = $row->业务;
-                $biz_arr['value'] = $row->业务;
-                $biz_arr['items'] = [];
+            if ($team_last != $row->班组 && $ii == (count($results)-1))
+            {
             }
         }
 
@@ -99,6 +128,9 @@ class Scheduling extends Controller
 
             for ($i=1; $i<=12; $i++)
             {
+                //$first_date = sprintf('%d-%02d-01',$year, $i);
+                //$last_date = date('Y-m-d', strtotime('$first_date + 1 month -1 day'));
+
                 $month_arr['id'] = sprintf('%d-%d', $year, $i);
                 $month_arr['value'] = sprintf('%d月', $i);
                 $month_arr['items'] = [];
@@ -132,10 +164,84 @@ class Scheduling extends Controller
         $job_arr['value'] = sprintf('吉林热线');
         array_push($task_arr, $job_arr);
 
+        // 班务
+        $sql = sprintf('
+            select 业务,班务编号,班务名称,班务时段,班务小时数 
+            from biz_duty_rule
+            group by 业务,班务编号');
+
+        $query = $model->select($sql);
+        $results = $query->getResult();
+
+        $duty_arr = array();
+
+        $biz_last = '';
+        $biz_arr = [];
+
+        $ii = 0;
+        foreach ($results as $row)
+        {
+            $ii ++;
+
+            if ($biz_last == '')
+            {
+                $biz_last = $row->业务;
+                $biz_arr['id'] = $row->业务;
+                $biz_arr['value'] = $row->业务;
+                $biz_arr['items'] = [];
+            }
+
+            $sch_arr = array();
+            $sch_arr['id'] = $row->班务编号;
+            $sch_arr['value'] = sprintf('%s (%s^%.1f)', $row->班务名称, $row->班务时段, $row->班务小时数);
+
+            if ($biz_last == $row->业务 && $ii < (count($results)-1))
+            {
+                array_push($biz_arr['items'], $sch_arr);
+            }
+            if ($biz_last == $row->业务 && $ii == (count($results)-1))
+            {
+                array_push($biz_arr['items'], $sch_arr);
+                array_push($duty_arr, $biz_arr);
+            }
+            if ($biz_last != $row->业务 && $ii < (count($results)-1))
+            {
+                array_push($duty_arr, $biz_arr);
+
+                $biz_last = $row->业务;
+                $biz_arr['id'] = $row->业务;
+                $biz_arr['value'] = $row->业务;
+                $biz_arr['items'] = [];
+                array_push($biz_arr['items'], $sch_arr);
+            }
+            if ($biz_last != $row->业务 && $ii == (count($results)-1))
+            {
+                array_push($duty_arr, $biz_arr);
+
+                $biz_last = $row->业务;
+                $biz_arr['id'] = $row->业务;
+                $biz_arr['value'] = $row->业务;
+                $biz_arr['items'] = [];
+                array_push($biz_arr['items'], $sch_arr);
+
+                array_push($duty_arr, $biz_arr);
+            }
+        }
+
+        $send['func_id'] = $menu_id;
         $send['csr_json'] = json_encode($csr_arr);
         $send['date_json'] = json_encode($date_arr);
         $send['task_json'] = json_encode($task_arr);
+        $send['duty_json'] = json_encode($duty_arr);
 
         echo view('VScheduling.php', $send);
+    }
+
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    // 排班设置
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    public function set($menu_id='')
+    {
+        $csr_arr = $this->request->getJSON(true);
     }
 }
--- app/Views/Vscheduling.php
+++ app/Views/Vscheduling.php
@@ -1,4 +1,4 @@
-<!-- v1.0.1.1.202205282355, from home -->
+<!-- v1.1.1.1.202205311600, from office -->
 <!DOCTYPE html>
 <html>
 
@@ -12,8 +12,8 @@
     <style type='text/css'>
         div.condtion_box
         {
-            width: 23%;
-            height: 550px;
+            width: 31%;
+            height: 570px;
             margin: 10px;
             background-color: #f9f9f9;
             border: 1px solid #D0D0D0;
@@ -23,7 +23,7 @@
         div.result_box
         {
             width: 100%;
-            height: 550px;
+            height: 570px;
             margin-top: 10px;
             background-color: #f9f9f9;
             border: 1px solid #D0D0D0;
@@ -38,7 +38,7 @@
     <div id='main_tb'></div>
     <div class='condtion_box' id='csrbox'><b>选择人员</b></div>
     <div class='condtion_box' id='datebox'><b>选择日期</b></div>
-    <div class='condtion_box' id='taskbox'><b>选择业务</b></div>
+    <!--<div class='condtion_box' id='taskbox'><b>选择业务</b></div>-->
     <div class='condtion_box' id='dutybox'><b>选择班务</b></div>
     <div class='result_box' id='resultbox'></div>
 
@@ -48,10 +48,12 @@
             return document.getElementById(id);
         }
 
+        $$('resultbox').style.display = 'none';
+
         // 生成主菜单栏
         var main_tb = new dhx.Toolbar('main_tb', {css:'toobar-class'});
         main_tb.data.add({id:'刷新', type:'button', value:'刷新'});
-        main_tb.data.add({id:'排班设置', type:'button', value:'排班设置'});
+        main_tb.data.add({id:'提交', type:'button', value:'提交'});
 
         var csr_obj = JSON.parse('<?php echo $csr_json; ?>');
         var csr_tree = new dhx.Tree('csrbox', {checkbox: true});
@@ -61,9 +63,15 @@
         var date_tree = new dhx.Tree('datebox', {checkbox: true});
         date_tree.data.parse(date_obj);
 
+        /*
         var task_obj = JSON.parse('<?php echo $task_json; ?>');
         var task_tree = new dhx.Tree('taskbox', {checkbox: true});
         task_tree.data.parse(task_obj);
+        */
+
+        var duty_obj = JSON.parse('<?php echo $duty_json; ?>');
+        var duty_tree = new dhx.Tree('dutybox', {checkbox: true});
+        duty_tree.data.parse(duty_obj);
 
         // 工具栏点击
         main_tb.events.on('click', function(id, e)
@@ -73,8 +81,32 @@
                 case '刷新':
                     window.location.reload();
                     break;
+                case '提交':
+                    tree_submit();
+                    break;
             }
         });
+
+        function tree_submit(id)
+        {
+            var sch_arr = [];
+            var csr_arr = csr_tree.getChecked();
+            var date_arr = date_tree.getChecked();
+            var duty_arr = duty_tree.getChecked();
+    
+            sch_arr['csr'] = csr_arr;
+            sch_arr['date'] = date_arr;
+            sch_arr['duty'] = duty_arr;
+
+            console.log(sch_arr);
+            dhx.ajax.post('<?php base_url(); ?>/Scheduling/set/<?php echo $func_id; ?>', sch_arr).then(function (data)
+            {
+                alert('新增记录成功');
+            }).catch(function (err)
+            {
+                alert('新增记录错误, ' + " " + err.statusText);
+            });
+        }
 
     </script>
 
