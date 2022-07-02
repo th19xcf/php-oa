ef1978c9634d6184527975cbc4114047340f4d2d
th19xcf <th19xcf@gmail.com>
2022-07-02 20:31:01 +0800

    2022-7-2

--- app/Controllers/Dept.php
+++ app/Controllers/Dept.php
@@ -1,5 +1,5 @@
 <?php
-/* v1.1.0.1.2022060221930, from home */
+/* v1.2.0.1.202207022020, from home */
 
 namespace App\Controllers;
 use \CodeIgniter\Controller;
@@ -15,17 +15,17 @@ class Dept extends Controller
     //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
     // 部门
     //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-    public function init($menu_id='')
+    public function init_1($menu_id='')
     {
         // 从session中取出数据
         $session = \Config\Services::session();
         $dept_authz = $session->get($menu_id.'-dept_authz');
 
         $sql = sprintf('
-            select 一级部门,二级部门,三级部门,四级部门,五级部门,六级部门
+            select 部门编码,一级部门,二级部门,三级部门,四级部门,五级部门,六级部门
             from view_dept
             where instr(部门编码,"%s")            
-                and instr(五级部门,"热线")
+                #and instr(五级部门,"热线")
             order by 一级部门,二级部门,三级部门,四级部门,五级部门,六级部门',
             $dept_authz);
 
@@ -50,8 +50,6 @@ class Dept extends Controller
         $items_5 = [];
         $items_6 = [];
 
-
-
         $ii = 0;
         $item = [];
 
@@ -63,8 +61,9 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['5级'] = '5级^' . $row->五级部门;
-                $item['id'] = '6级^' . $row->六级部门;
+                $item['id'] = '6级^' . $row->六级部门 . '^' . $row->部门编码;
                 $item['value'] = $row->六级部门;
+                $item['guid'] = $row->部门编码;
 
                 $items_6[$row->六级部门] = $item;
             }
@@ -72,8 +71,9 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['4级'] = '4级^' . $row->四级部门;
-                $item['id'] = '5级^' . $row->五级部门;
+                $item['id'] = '5级^' . $row->五级部门 . '^' . $row->部门编码;
                 $item['value'] = $row->五级部门;
+                $item['guid'] = $row->部门编码;
 
                 $items_5[$row->五级部门] = $item;
             }
@@ -81,8 +81,9 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['3级'] = '3级^' . $row->三级部门;
-                $item['id'] = '4级^' . $row->四级部门;
+                $item['id'] = '4级^' . $row->四级部门 . '^' . $row->部门编码;
                 $item['value'] = $row->四级部门;
+                $item['guid'] = $row->部门编码;
 
                 $items_4[$row->四级部门] = $item;
             }
@@ -90,8 +91,9 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['2级'] = '2级^' . $row->二级部门;
-                $item['id'] = '3级^' . $row->三级部门;
+                $item['id'] = '3级^' . $row->三级部门 . '^' . $row->部门编码;
                 $item['value'] = $row->三级部门;
+                $item['guid'] = $row->部门编码;
 
                 $items_3[$row->三级部门] = $item;
             }
@@ -99,8 +101,9 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['1级'] = '1级^' . $row->一级部门;
-                $item['id'] = '2级^' . $row->二级部门;
+                $item['id'] = '2级^' . $row->二级部门 . '^' . $row->部门编码;
                 $item['value'] = $row->二级部门;
+                $item['guid'] = $row->部门编码;
 
                 $items_2[$row->二级部门] = $item;
             }
@@ -108,7 +111,8 @@ class Dept extends Controller
             {
                 $item = [];
                 $item['id'] = '1级^' . $row->一级部门;
-                $item['value'] = $row->一级部门;
+                $item['value'] = $row->一级部门 . '^' . $row->部门编码;
+                $item['guid'] = $row->部门编码;
 
                 $items_1[$row->一级部门] = $item;
             }
@@ -118,13 +122,13 @@ class Dept extends Controller
         {
             $dept = [];
             $dept['4级'] = $value['4级'];
-            $dept['id'] = '5级^' . $key;
+            $dept['id'] = $value['id'];
             $dept['value'] = $key;
-
             $dept['items'] = [];
+
             foreach ($items_6 as $kk => $vv)
             {
-                if ($dept['id'] == $vv['5级'])
+                if ($vv['5级'] == ('5级^' . $dept['value']))
                 {
                     array_push($dept['items'], $vv);
                 }
@@ -137,13 +141,13 @@ class Dept extends Controller
         {
             $dept = [];
             $dept['3级'] = $value['3级'];
-            $dept['id'] = '4级^' . $key;
+            $dept['id'] = $value['value'];
             $dept['value'] = $key;
-
             $dept['items'] = [];
+
             foreach ($dept_5_arr as $kk => $vv)
             {
-                if ($dept['id'] == $vv['4级'])
+                if ($vv['4级'] == ('4级^' . $dept['value']))
                 {
                     array_push($dept['items'], $vv);
                 }
@@ -156,13 +160,13 @@ class Dept extends Controller
         {
             $dept = [];
             $dept['2级'] = $value['2级'];
-            $dept['id'] = '3级^' . $key;
+            $dept['id'] = $value['id'];;
             $dept['value'] = $key;
-
             $dept['items'] = [];
+
             foreach ($dept_4_arr as $kk => $vv)
             {
-                if ($dept['id'] == $vv['3级'])
+                if ($vv['3级'] == ('3级^' . $dept['value']))
                 {
                     array_push($dept['items'], $vv);
                 }
@@ -175,13 +179,13 @@ class Dept extends Controller
         {
             $dept = [];
             $dept['1级'] = $value['1级'];
-            $dept['id'] = '2级^' . $key;
+            $dept['id'] = $value['id'];;
             $dept['value'] = $key;
-
             $dept['items'] = [];
+
             foreach ($dept_3_arr as $kk => $vv)
             {
-                if ($dept['id'] == $vv['2级'])
+                if ($vv['2级'] == ('2级^' . $dept['value']))
                 {
                     array_push($dept['items'], $vv);
                 }
@@ -193,13 +197,13 @@ class Dept extends Controller
         foreach ($items_1 as $key => $value)
         {
             $dept = [];
-            $dept['id'] = '1级^' . $key;
+            $dept['id'] = $value['id'];;
             $dept['value'] = $key;
-
             $dept['items'] = [];
+
             foreach ($dept_2_arr as $kk => $vv)
             {
-                if ($dept['id'] == $vv['1级'])
+                if ($vv['1级'] == ('1级^' . $dept['value']))
                 {
                     array_push($dept['items'], $vv);
                 }
@@ -210,6 +214,185 @@ class Dept extends Controller
 
         $send['func_id'] = $menu_id;
         $send['dept_json'] = json_encode($dept_1_arr);
+
+        echo view('Vdept.php', $send);
+    }
+
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    // 部门
+    //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+    public function init($menu_id='')
+    {
+        // 从session中取出数据
+        $session = \Config\Services::session();
+        $dept_authz = $session->get($menu_id.'-dept_authz');
+
+        $sql = sprintf('
+            select 部门编码,部门名称,部门级别,上级部门
+            from def_dept
+            where instr(部门编码,"%s")
+            order by 部门编码',
+            $dept_authz);
+
+        $model = new Mcommon();
+
+        $query = $model->select($sql);
+        $results = $query->getResult();
+
+        $dept = '';
+        $dept_1_arr = [];
+        $dept_2_arr = [];
+        $dept_3_arr = [];
+        $dept_4_arr = [];
+        $dept_5_arr = [];
+
+        $ii = 0;
+        $item_level = [];
+
+        for ($ii=0; $ii<7; $ii++)
+        {
+            $item_level[$ii] = [];
+        }
+
+        foreach ($results as $row)
+        {
+            $ii ++;
+
+            $item = [];
+            $item['id'] = sprintf('%d级^%s^%s', $row->部门级别, $row->部门名称, $row->部门编码);
+            $item['value'] = $row->部门名称;
+            $item['guid'] = $row->部门编码;
+            $item['level'] = $row->部门级别;
+            $item['higher'] = $row->上级部门;
+
+            array_push($item_level[$row->部门级别], $item);
+        }
+
+        foreach ($item_level[5] as $item)
+        {
+            $dept = [];
+            $dept['id'] = $item['id'];
+            $dept['value'] = $item['value'];
+            $dept['higher'] = $item['higher'];
+            $dept['items'] = [];
+
+            foreach ($item_level[6] as $child)
+            {
+                if ($child['higher'] == $item['guid'])
+                {
+                    array_push($dept['items'], $child);
+                }
+            }
+
+            $dept['value'] = sprintf('%s (%d)',$item['value'], count($dept['items']));
+            array_push($dept_5_arr, $dept);
+        }
+
+        foreach ($item_level[4] as $item)
+        {
+            $dept = [];
+            $dept['id'] = $item['id'];
+            $dept['value'] = $item['value'];
+            $dept['higher'] = $item['higher'];
+            $dept['items'] = [];
+
+            foreach ($dept_5_arr as $child)
+            {
+                if ($child['higher'] == $item['guid'])
+                {
+                    array_push($dept['items'], $child);
+                }
+            }
+
+            $dept['value'] = sprintf('%s (%d)',$item['value'], count($dept['items']));
+            array_push($dept_4_arr, $dept);
+        }
+
+        foreach ($item_level[3] as $item)
+        {
+            $dept = [];
+            $dept['id'] = $item['id'];
+            $dept['value'] = $item['value'];
+            $dept['higher'] = $item['higher'];
+            $dept['items'] = [];
+
+            foreach ($dept_4_arr as $child)
+            {
+                if ($child['higher'] == $item['guid'])
+                {
+                    array_push($dept['items'], $child);
+                }
+            }
+
+            $dept['value'] = sprintf('%s (%d)',$item['value'], count($dept['items']));
+            array_push($dept_3_arr, $dept);
+        }
+
+        if (count($dept_3_arr) > 0) $dept_arr = $dept_3_arr;
+
+        foreach ($item_level[2] as $item)
+        {
+            $dept = [];
+            $dept['id'] = $item['id'];
+            $dept['value'] = $item['value'];
+            $dept['higher'] = $item['higher'];
+            $dept['items'] = [];
+
+            foreach ($dept_3_arr as $child)
+            {
+                if ($child['higher'] == $item['guid'])
+                {
+                    array_push($dept['items'], $child);
+                }
+            }
+
+            $dept['value'] = sprintf('%s (%d)',$item['value'], count($dept['items']));
+            array_push($dept_2_arr, $dept);
+        }
+
+        if (count($dept_2_arr) > 0) $dept_arr = $dept_2_arr;
+
+        foreach ($item_level[1] as $item)
+        {
+            $dept = [];
+            $dept['id'] = $item['id'];
+            $dept['value'] = $item['value'];
+            $dept['higher'] = $item['higher'];
+            $dept['items'] = [];
+
+            foreach ($dept_2_arr as $child)
+            {
+                if ($child['higher'] == $item['guid'])
+                {
+                    array_push($dept['items'], $child);
+                }
+            }
+
+            $dept['value'] = sprintf('%s (%d)',$item['value'], count($dept['items']));
+            array_push($dept_1_arr, $dept);
+        }
+
+        if (count($dept_1_arr) > 0) $dept_arr = $dept_1_arr;
+
+        /*
+        $dept = [];
+        $dept['id'] = '1级';
+        $dept['value'] = '电发';
+        $item['higher'] = '';
+        $dept['items'] = [];
+
+        $item = [];
+        $item['id'] = '2级';
+        $item['value'] = '呼叫中心';
+        $item['higher'] = '';
+        array_push($dept['items'], $item);
+
+        $dept_arr = [];
+        array_push($dept_arr, $dept);
+        */
+
+        $send['func_id'] = $menu_id;
+        $send['dept_json'] = json_encode($dept_arr);
 
         echo view('Vdept.php', $send);
     }
--- app/Views/Vdept.php
+++ app/Views/Vdept.php
@@ -1,4 +1,4 @@
-<!-- v1.1.0.1.2022060221930, from home -->
+<!-- v1.2.1.1.202207022020, from home -->
 <!DOCTYPE html>
 <html>
 
@@ -9,11 +9,16 @@
     <link rel='stylesheet' type='text/css' href='<?php base_url(); ?>/dhtmlx/codebase/suite.css'>
     <script src='<?php base_url(); ?>/dhtmlx/codebase/suite.js'></script>
 
+    <link rel='stylesheet' type='text/css' href='<?php base_url(); ?>/ag-grid/dist/styles/ag-grid.css'>
+    <link rel='stylesheet' type='text/css' href='<?php base_url(); ?>/ag-grid/dist/styles/ag-theme-alpine.css'>
+    <script src='<?php base_url(); ?>/ag-grid/dist/ag-grid-locale-cn.js'></script>
+    <script src='<?php base_url(); ?>/ag-grid/dist/ag-grid-community.noStyle.js'></script>
+
     <style type='text/css'>
-        div.condtion_box
+        div.float_box
         {
             width: 46%;
-            height: 570px;
+            height: 640px;
             margin: 10px;
             background-color: #f9f9f9;
             border: 1px solid #D0D0D0;
@@ -21,12 +26,17 @@
             float: left;
         }
     </style>
-
 </head>
 
 <body>
-    <div id='main_tb'></div>
-    <div class='condtion_box' id='deptbox'><b>组织架构</b></div>
+    <div id='main_tb' ></div>
+    <div class='float_box'>
+        <div id='tree_box' style='height:95%;'></div>
+    </div>
+
+    <div class='float_box'>
+        <div id='grid_box' class='ag-theme-alpine' style='width:100%; height:91.5%; background-color:lightblue;'></div>
+    </div>
 
     <script type='text/javascript' charset='utf-8'>
         function $$(id)
@@ -34,14 +44,44 @@
             return document.getElementById(id);
         }
 
-        // 生成主菜单栏
+        // 变量
+        var selected_id = '';
+
+        // tree视图
         var main_tb = new dhx.Toolbar('main_tb', {css:'toobar-class'});
         main_tb.data.add({id:'刷新', type:'button', value:'刷新'});
+        main_tb.data.add({type:'separator'});
+        main_tb.data.add({id:'修改部门名称', type:'button', value:'修改部门名称'});
+        main_tb.data.add({id:'新增下级部门', type:'button', value:'新增下级部门'});
+        main_tb.data.add({id:'删除部门', type:'button', value:'删除部门'});
+        main_tb.data.add({type:'separator'});
         main_tb.data.add({id:'提交', type:'button', value:'提交'});
 
         var dept_obj = JSON.parse('<?php echo $dept_json; ?>');
-        var dept_tree = new dhx.Tree('deptbox', {checkbox: true});
+        //var dept_tree = new dhx.Tree('tree_box', {checkbox: true});
+        var dept_tree = new dhx.Tree('tree_box');
         dept_tree.data.parse(dept_obj);
+
+        //grid视图
+        const grid_options = 
+        {
+            columnDefs: 
+            [
+                {field:'表项'},
+                {field:'值'},
+            ],
+            defaultColDef: 
+            {
+                resizable: true,
+            },
+            rowData:
+            [
+                {'表项':'部门名称', '值':''},
+                {'表项':'上级部门', '值':''},
+            ]
+        };
+
+        new agGrid.Grid($$('grid_box'), grid_options);
 
         // 工具栏点击
         main_tb.events.on('click', function(id, e)
@@ -54,12 +94,65 @@
                 case '提交':
                     upkeep_submit();
                     break;
+                case '删除部门':
+                    console.log('id=', selected_id);
+                    delete_item();
+                    break;
             }
         });
+
+        function delete_item()
+        {
+            if (selected_id == '')
+            {
+                alert('请先选择部门');
+                return;
+            }
+            if (dept_tree.data.haveItems(selected_id))
+            {
+                alert('"' + selected_id + '"' + ' 有下级部门,不能删除');
+                return;
+            }
+            if (confirm('是否删除'))
+            {
+                alert('删除');
+            }
+        }
 
         function upkeep_submit(id)
         {
         }
+
+        //tree event
+        dept_tree.events.on('itemClick', function(id, e)
+        {
+            selected_id = id;
+
+            var item = id.split('^');
+
+            var items = [];
+            items.push({'表项':'部门编码', '值':item[2]});
+            items.push({'表项':'部门名称', '值':item[1]});
+            items.push({'表项':'部门级别', '值':item[0]});
+            if (item[0] == '1级')
+            {
+                items.push({'表项':'上级部门', '值':'无'});
+            }
+            else
+            {
+                items.push({'表项':'上级部门', '值':dept_tree.data.getParent(id).split('^')[1]});
+            }
+            if (dept_tree.data.haveItems(id))
+            {
+                items.push({'表项':'下级部门', '值':'有'});
+            }
+            else
+            {
+                items.push({'表项':'下级部门', '值':'无'});
+            }
+
+            grid_options.api.setRowData(items);
+        });
 
     </script>
 
